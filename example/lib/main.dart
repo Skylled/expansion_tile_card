d12f034a80a00d594c823238b12b50a5c34d9d13
Kyle Bradshaw <TehSkull@hotmail.com>
2020-07-30 22:25:58 -0500

    Rework example to show controlling another card Issue #10

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,95 +26,200 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  final GlobalKey<ExpansionTileCardState> cardA = new GlobalKey();
+  final GlobalKey<ExpansionTileCardState> cardB = new GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-          child: ExpansionTileCard(
-            leading: CircleAvatar(child: Text('$index')),
-            title: Text('This is item #$index'),
-            subtitle: Text('Tap to see more!'),
-            children: <Widget>[
-              Divider(
-                thickness: 1.0,
-                height: 1.0,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                    vertical: 8.0,
-                  ),
-                  child: Text(
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: ExpansionTileCard(
+              key: cardA,
+              leading: CircleAvatar(child: Text('A')),
+              title: Text('Tap me!'),
+              subtitle: Text('I expand!'),
+              children: <Widget>[
+                Divider(
+                  thickness: 1.0,
+                  height: 1.0,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Text(
 """Hi there, I'm a drop-in replacement for Flutter's ExpansionTile.
 
-Use me any time you think your app could benefit from being just a bit more Material.""",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2
-                        .copyWith(fontSize: 16),
+Use me any time you think your app could benefit from being just a bit more Material.
+
+These buttons control the next card down!""",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(fontSize: 16),
+                    ),
                   ),
                 ),
-              ),
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceAround,
-                buttonHeight: 52.0,
-                buttonMinWidth: 90.0,
-                children: <Widget>[
-                  FlatButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0)),
-                    onPressed: () {},
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.star),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 2.0),
-                        ),
-                        Text('Save'),
-                      ],
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceAround,
+                  buttonHeight: 52.0,
+                  buttonMinWidth: 90.0,
+                  children: <Widget>[
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardB.currentState?.expand();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.arrow_downward),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Open'),
+                        ],
+                      ),
                     ),
-                  ),
-                  FlatButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0)),
-                    onPressed: () {},
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.open_in_browser),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 2.0),
-                        ),
-                        Text('Open'),
-                      ],
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardB.currentState?.collapse();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.arrow_upward),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Close'),
+                        ],
+                      ),
                     ),
-                  ),
-                  FlatButton(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4.0)),
-                    onPressed: () {},
-                    child: Column(
-                      children: <Widget>[
-                        Icon(Icons.share),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 2.0),
-                        ),
-                        Text('Share'),
-                      ],
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardB.currentState?.toggleExpansion();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.swap_vert),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Toggle'),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
-        );
-      }),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: ExpansionTileCard(
+              key: cardB,
+              leading: CircleAvatar(child: Text('B')),
+              title: Text('Tap me!'),
+              subtitle: Text('I expand, too!'),
+              children: <Widget>[
+                Divider(
+                  thickness: 1.0,
+                  height: 1.0,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Text(
+"""Hi there, I'm a drop-in replacement for Flutter's ExpansionTile.
+
+Use me any time you think your app could benefit from being just a bit more Material.
+
+These buttons control the card above!""",
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2
+                          .copyWith(fontSize: 16),
+                    ),
+                  ),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceAround,
+                  buttonHeight: 52.0,
+                  buttonMinWidth: 90.0,
+                  children: <Widget>[
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardA.currentState?.expand();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.arrow_downward),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Open'),
+                        ],
+                      ),
+                    ),
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardA.currentState?.collapse();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.arrow_upward),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Close'),
+                        ],
+                      ),
+                    ),
+                    FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      onPressed: () {
+                        cardA.currentState?.toggleExpansion();
+                      },
+                      child: Column(
+                        children: <Widget>[
+                          Icon(Icons.swap_vert),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2.0),
+                          ),
+                          Text('Toggle'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
